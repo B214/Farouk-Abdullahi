b5131b3e7f66ebe07b04d2718c9fea2a16f94c0e
B214 <64994763+B214@users.noreply.github.com>
2021-04-04 23:29:59 +0100

    Add files via upload

---
+++ csc104.py
@@ -0,0 +1,34 @@
+
+# Number 1
+name = 'Farouk & ' 
+department = 'computer_science '
+print(name +  department)
+# Number 2
+scare= "boo!"
+print( scare)
+# Number 3
+a=7
+b=5
+sum=a+b
+print(sum)
+print("a", "b", "sum")
+# Number 4
+x= 12
+y=.5
+divide= x / y
+print (divide)
+#Number 5
+num1=8
+num2=3
+sum=num1 + num2
+print (sum)
+# Number 6
+x=8
+y=3
+remender= x % y
+print(remender)
+# Number 7
+First= 2 * 4
+Second= (4 + 2)
+result= (First * Second)
+print(result)
